99e046626ae9d0dd457eca91ad5a7f90645250bb
nebirhos <me@nebirhos.com>
2013-06-20 16:21:50 +0200

    Rspec update and setup

--- Gemfile
+++ Gemfile
@@ -1,6 +1,13 @@
 source 'http://rubygems.org'
 
-# Provides basic authentication functionality for testing parts of your engine
-gem 'spree_auth_devise', :git => "git://github.com/spree/spree_auth_devise", :branch => "1-3-stable"
+# # Provides basic authentication functionality for testing parts of your engine
+# gem 'spree_auth_devise', :git => "git://github.com/spree/spree_auth_devise", :branch => "1-3-stable"
+
+group :test do
+  gem 'capybara-webkit'
+  gem 'selenium-webdriver', '~> 2.32'
+  gem 'rspec-rails', '~> 2.13'
+  gem 'database_cleaner', '0.7.1'
+end
 
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,27 +8,24 @@ require 'ffaker'
 require 'pry-rails'
 require 'pry-debugger'
 
+require 'capybara/rspec'
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+Capybara.javascript_driver = :chrome
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
 
 # Requires factories defined in spree_core
 require 'spree/core/testing_support/factories'
+require 'spree/core/testing_support/capybara_ext'
 require 'spree/core/testing_support/controller_requests'
 require 'spree/core/testing_support/authorization_helpers'
 require 'spree/core/url_helpers'
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-
-  # == URL Helpers
-  #
-  # Allows access to Spree's routes in specs:
-  #
-  # visit spree.admin_path
-  # current_path.should eql(spree.products_path)
-  config.include Spree::Core::UrlHelpers
-
   # == Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -44,5 +41,26 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
+
+  config.before(:each) do
+    if example.metadata[:js]
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+    # reset_spree_preferences
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+
+  config.include Spree::Core::UrlHelpers
+  config.include FactoryGirl::Syntax::Methods
 end
